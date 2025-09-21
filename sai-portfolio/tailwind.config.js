811691fe07da40936914542e73bf835056dcbb8d
saikireeti40 <gurijala@usc.edu>
2025-09-20 19:21:46 -0700

    Update tailwind.config.js

--- sai-portfolio/tailwind.config.js
+++ sai-portfolio/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       fontFamily: {
-        display: ["ui-sans-serif","system-ui","-apple-system","Segoe UI","Roboto","Inter","Helvetica Neue","Arial","Noto Sans","Apple Color Emoji","Segoe UI Emoji"]
+        display: [
+          "ui-sans-serif","system-ui","-apple-system","Segoe UI","Roboto",
+          "Inter","Helvetica Neue","Arial","Noto Sans","Apple Color Emoji","Segoe UI Emoji"
+        ]
       }
     },
   },
